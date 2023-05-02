586a7e1cdd3d16352ac5fa5e8617edb6aadd6382
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-05-02 16:10:26 +0300

    555

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import user from 'components/data/user.json';
+import data from 'components/data/data.json';
+import friends from 'components/data/friends.json';
+import transactions from 'components/data/transactions.json';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import { Box } from './Profile/Profile.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Box>
   );
 };
